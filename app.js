7e567441d302f51b3eeb5e20f3e49bd11a9b751b
Jasper Chen <jasperdyb@gmail.com>
2020-02-16 13:15:02 +0800

    dev_1
    2020/2/16 13:14

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+const restaurant_list = require('./restaurant.json')
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'Main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routers
+app.get('/', (req, res) => {
+  let key = null
+  res.render('index', { restaurants: restaurant_list.results, keyword: key })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurant_list.results.find(data => req.params.id === data.id.toString())
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const key = req.query.keyword
+  const restaurants = restaurant_list.results.filter(data => {
+    return data.name.toLowerCase().includes(key.toLowerCase()) || data.category.toLowerCase().includes(key.toLowerCase())
+  })
+
+  res.render('index', { restaurants: restaurants, keyword: key })
+})
+
+// server start
+app.listen(port, () => {
+  console.log(`server started.`)
+})
