7c9b791e8feb00c84b1fda09fc56b0533addf39f
Jasper Chen <jasperdyb@gmail.com>
2020-02-28 00:29:26 +0800

    Add deletion confirm modal

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,24 @@ let deleteButton = document.getElementById("delete_button")
 
 
 function toggleDeletion() {
-  console.log($(".delete-button"))
+  // console.log($(".delete-button"))
 
-  $(".delete-button").toggle(function () {
+  $(".delete-button-form").toggle(function () {
     $("#user_button").css({ display: 'none' });
   }, function () {
     $("#user_button").css({ display: 'block' });
   });
 }
+
+$('.delete-button').click(function (e) {
+  e.stopPropagation();
+  console.log($(e.currentTarget).data('restaurant_id'))
+  $("#deletion").modal();
+});
+
+$(document).ready(function () {
+  jQuery('[data-toggle="tooltip"]').tooltip();
+});
+
 
 deleteButton.addEventListener('click', toggleDeletion)
