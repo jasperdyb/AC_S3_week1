bb03d9dfd64e4533d75fc18fcb5136536c25a770
Jasper Chen <jasperdyb@gmail.com>
2020-03-07 21:47:00 +0800

    Layout login & register page

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs')
 
 // 登入頁面
 router.get('/login', (req, res) => {
-
+  res.render('login')
 })
 
 // 登入檢查
@@ -16,7 +16,7 @@ router.post('/login', (req, res, next) => {
 
 // 註冊頁面
 router.get('/register', (req, res) => {
-
+  res.render('register')
 })
 
 // 註冊檢查
